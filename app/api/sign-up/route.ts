e4ceb71f1ad3a673b60774cc53fec133593b7092
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-05 10:44:39 +0600

    updated the responce ststus

--- app/api/sign-up/route.ts
+++ app/api/sign-up/route.ts
@@ -85,7 +85,7 @@ export async function POST(request: Request) {
                 success: true,
                 message: "User registered successfully"
             },{
-                status:400
+                status:200
             }
         )
 
