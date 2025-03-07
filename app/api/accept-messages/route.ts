93f788ac4c22673bc8c7350254eac735edcc4b64
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-03-07 09:52:50 +0600

    created the get meessage

--- app/api/accept-messages/route.ts
+++ app/api/accept-messages/route.ts
@@ -68,3 +68,58 @@ export async function POST(request: Request) {
     }
 
 }
+
+export async function GET(request: Request) {
+    await mongoDBConnecnt();
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
+
+    if( !session || !user){
+        return Response.json(
+            {
+                success: false,
+                message: "Not Authenticated"
+            },
+            {
+                status: 401
+            }
+        )
+    };
+
+    const userId = user._id;
+
+    try {
+        const findedUser = await UserModel.findById(userId);
+        if (!findedUser) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found"
+                },
+                {
+                    status: 404
+                }
+            )
+        }
+    
+        return Response.json(
+            {
+                success: true,
+                isAcceptingMessage: findedUser.isAcceptingMessage
+            },
+            {
+                status: 200
+            }
+        )
+    } catch (error) {
+        return Response.json(
+            {
+                success: false,
+                message: "Error in getting message acceptance"
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
