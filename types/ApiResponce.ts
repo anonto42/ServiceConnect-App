2c3dfcaebb29c6be5c1d46392b17b828d5b89e3c
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-10 17:10:17 +0600

    new backend init

--- types/ApiResponce.ts
+++
@@ -1,8 +0,0 @@
-import { messageType } from "@/model/User";
-
-export interface ApiResponce {
-    success: boolean;
-    message: string;
-    isAccesptingMessages?: boolean;
-    messages?: Array<messageType>
-}
